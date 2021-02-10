1a49e8e1adf55c54db00ed33609890cffb349f0f
Steven Li <60322211+stevenli5@users.noreply.github.com>
2021-02-10 15:26:53 -0500

    Added ReplaceMathML

---
+++ ReplaceMathML.vb
@@ -0,0 +1,42 @@
+Sub ReplaceMathML()
+
+Dim nNumber As Integer
+Dim mNumber As Integer
+Dim strMath As String
+Dim objData As New MSForms.DataObject
+Dim oRange As Range
+Dim prevString As String
+Dim nextString As String
+
+If ActiveDocument.OMaths.Count > 0 Then
+
+    With ActiveDocument
+        For nNumber = 1 To .OMaths.Count Step 1
+            .OMaths(1).Range.Select
+            Set oRange = .OMaths(1).Range
+            Selection.Copy
+            objData.GetFromClipboard
+            strMath = Replace(objData.GetText(), "mml:", "")
+            strMath = Replace(strMath, "xmlns:mml=""http://www.w3.org/1998/Math/MathML"" xmlns:m=""http://schemas.openxmlformats.org/officeDocument/2006/math""", "")
+            
+            oRange.Select
+            prevString = Selection.Previous
+            nextString = Selection.Next
+            Selection.Next.Select
+            .OMaths(1).Range.Select
+            Selection.Delete
+            Selection.EndOf
+            oRange.Select
+            Selection.Previous.Select
+            Selection.InsertAfter (" " + strMath + " ")
+
+        Next nNumber
+    End With
+
+Else
+    
+    MsgBox ("No math")
+    
+End If
+
+End Sub
