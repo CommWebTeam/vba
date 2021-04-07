1f5034d8f6aba536dd6c1821a8220cb6e8eb79e3
Steven Li <60322211+stevenli5@users.noreply.github.com>
2021-04-07 11:10:59 -0400

    Added support for math inside tables

--- ReplaceMathML.vb
+++ ReplaceMathML.vb
@@ -6,7 +6,6 @@ Dim objData As New MSForms.DataObject
 Dim oRange As Range
 
 If ActiveDocument.OMaths.Count > 0 Then
-
     With ActiveDocument
         For nNumber = 1 To .OMaths.Count Step 1
             .OMaths(1).Range.Select
@@ -15,23 +14,20 @@ If ActiveDocument.OMaths.Count > 0 Then
             objData.GetFromClipboard
             strMath = Replace(objData.GetText(), "mml:", "")
             strMath = Replace(strMath, "xmlns:mml=""http://www.w3.org/1998/Math/MathML"" xmlns:m=""http://schemas.openxmlformats.org/officeDocument/2006/math""", "")
-            
             oRange.Select
             Selection.Next.Select
             .OMaths(1).Range.Select
             Selection.Delete
             Selection.EndOf
             oRange.Select
-            Selection.Previous.Select
             Selection.InsertAfter (" " + strMath + " ")
-
         Next nNumber
+
     End With
 
 Else
-    
     MsgBox ("No math")
-    
+
 End If
 
 End Sub
